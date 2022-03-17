15aba2d762c40fb785d34d1a35c72f9601e20c07
NicoCam27 <yoovalcamjalli@gmail.com>
2022-03-17 19:31:58 +0100

    lab 8 reading

--- lab08-sprites/sprite_test.py
+++ lab08-sprites/sprite_test.py
@@ -21,8 +21,8 @@ class MyGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
         # Variables that will hold sprite lists
-        self.coin_list = None
         self.player_list = None
+        self.coin_list = None
 
         # Set up the player info
         self.player_sprite = None
@@ -67,16 +67,35 @@ class MyGame(arcade.Window):
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-
-        # Draw the sprite lists here. Typically sprites are divided into
-        # different groups. Other game engines might call these "sprite layers"
-        # or "sprite groups." Sprites that don't move should be drawn in their
-        # own group for the best performance, as Arcade can tell the graphics
-        # card to just redraw them at the same spot.
-        # Try to avoid drawing sprites on their own, and not in a layer.
-        # There are many performance improvements to drawing in a layer.
         self.coin_list.draw()
         self.player_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """ Handle Mouse Motion """
+
+        # Move the center of the player sprite to match the mouse x, y
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.coin_list.update()
+
+        # Generate a list of all sprites that collided with the player.
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.coin_list)
+
+        # Loop through each colliding sprite, remove it, and add to the score.
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
 
 
 def main():
