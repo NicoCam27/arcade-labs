35870b3a0e6a85e3b1bc7fcfd3e87fff5e8b14ca
NicoCam27 <yoovalcamjalli@gmail.com>
2022-04-05 20:58:59 +0200

    moving animation

---
+++ Python messing around and tests 2/personaje.py
@@ -0,0 +1,20 @@
+# Clase personaje (Clase Padre, deciende de la clase sprite)
+# Que tenga getters y setters.
+# Getters de posicion.
+# Movimiento (W.A.S.D para el protagoinsta, y para los demás personajes su IA)
+
+# Ataque (un metodo de ataque)
+# Vida (Puntos de vida, cambios de animaciones al ser golpeado?)
+
+
+# Clase Protagonista (Clase hija)
+# Que tenga getters y setters.
+# Getters de posicion.
+# Que herede el movimiento.
+# Ataque - puede tener varios tipos de movimientos.
+
+# Clase enemigo (Clase hija).
+# Eso ya es para adelante.
+
+
+#Para el lunes, pantalla vacia con un sprite que se mueva bien
