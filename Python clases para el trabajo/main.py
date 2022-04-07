801c2170053142e7df2a801f5a80c11b05572632
NicoCam27 <yoovalcamjalli@gmail.com>
2022-04-08 01:56:03 +0200

    demo para lunes de movimiento

--- Python clases para el trabajo/main.py
+++ Python clases para el trabajo/main.py
@@ -1,16 +1,10 @@
 
-import math
-import random
 import arcade
-from typing import Optional
-import time
 import personaje # Aquí se importa la clase de personaje
-
-from arcade.pymunk_physics_engine import PymunkPhysicsEngine
 
 SCREEN_TITLE = "Juego de equipo Durum studio"
 SPRITE_SCALING_PLAYER = 0.5
-MOVEMENT_SPEED = 5
+MOVEMENT_SPEED = 3
 
 SPRITE_IMAGE_SIZE = 128
 SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
@@ -19,61 +13,51 @@ SCREEN_WIDTH = SPRITE_SIZE * 15
 SCREEN_HEIGHT = SPRITE_SIZE * 10
 
 
-PLAYER_MOVE_FORCE = 4000
-
 
 class MyWindow(arcade.Window):
     """ Main Window """
-    def __init__(self, width, height, title, protagonista):
+    def __init__(self, width, height, title):
         """ Init """
         super().__init__(width, height, title)
-
+        self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.BLACK)
-        self.physics_engine: Optional[PymunkPhysicsEngine] = None
-        self.personaje = protagonista
+        self.personaje_principal = personaje.Protagonista(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,0,0, ":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING_PLAYER)
 
     def setup(self):
         """ Set up everything """
-
-
-        # --- Pymunk Physics Engine Setup ---
-
-        # The default damping for every object controls the percent of velocity
-        # the object will keep each second. A value of 1.0 is no speed loss,
-        # 0.9 is 10% per second, 0.1 is 90% per second.
-        # For top-down games, this is basically the friction for moving objects.
-        # For platformers with gravity, this should probably be set to 1.0.
-        # Default value is 1.0 if not specified.
-        damping = 0.7
-
-        # Set the gravity. (0, 0) is good for outer space and top-down.
-        gravity = (0, 0)
-
-        # Create the physics engine
-        self.physics_engine = PymunkPhysicsEngine(damping=damping,
-                                                  gravity=gravity)
-
-    def on_key_press(self, symbol: int, modifiers: int):
-        self.personaje.on_key_press()
-
-    def on_key_release(self, symbol: int, modifiers: int):
-        self.personaje.on_key_release()
-
-    def on_update(self, delta_time):
-        """ Movement and game logic """
-        # --- Move items in the physics engine
-        self.physics_engine.step()
-        self.personaje.on_update(delta_time)
+        self.personaje_principal.setup()
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        self.personaje.on_draw()
+        self.personaje_principal.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+        self.personaje_principal.update()
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            self.personaje_principal.set_change_x(-MOVEMENT_SPEED)
+        elif key == arcade.key.RIGHT:
+            self.personaje_principal.set_change_x(MOVEMENT_SPEED)
+        elif key == arcade.key.UP:
+            self.personaje_principal.set_change_y(MOVEMENT_SPEED)
+        elif key == arcade.key.DOWN:
+            self.personaje_principal.set_change_y(-MOVEMENT_SPEED)
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.personaje_principal.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.personaje_principal.change_y = 0
+
 
 def main():
     """ Main method """
-    juana = personaje.Protagonista(":resources:images/animated_characters/female_person/femalePerson_idle.png")
-    window = MyWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, juana)
+    window = MyWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 
--- Python clases para el trabajo/personaje.py
+++ Python clases para el trabajo/personaje.py
@@ -20,8 +20,6 @@
 # Para el lunes, pantalla vacia con un sprite que se mueva bien
 
 import arcade
-from typing import Optional
-from arcade.pymunk_physics_engine import PymunkPhysicsEngine
 
 SPRITE_SCALING_PLAYER = 0.5
 MOVEMENT_SPEED = 5
@@ -32,32 +30,36 @@ SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
 SCREEN_WIDTH = SPRITE_SIZE * 15
 SCREEN_HEIGHT = SPRITE_SIZE * 10
 
-# Physics force used to move the player. Higher number, faster accelerating.
-PLAYER_MOVE_FORCE = 4000
-BULLET_MOVE_FORCE = 2500
-
-PLAYER_MOVE_FORCE = 4000
 
 
-class Personaje(arcade.Sprite):
+class Protagonista(arcade.Sprite):
 
-    def __init__(self, sprite):
+    def __init__(self, position_x, position_y, change_x, change_y, sprite, size):
         super().__init__()
-        self.player_list = None
-        self.player_sprite = arcade.Sprite(sprite, SPRITE_SCALING_PLAYER)
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.player_sprite = arcade.Sprite(sprite, size)
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
         self.hp = None
 
     def setup(self):
-        self.player_list = arcade.SpriteList()
-        self.player_sprite.center_x = 250
-        self.player_sprite.center_y = 250
-        self.player_list.append(self.player_sprite)
-        self.physics_engine.add_sprite(self.player_sprite,
-                                       friction=0.6,
-                                       moment=PymunkPhysicsEngine.MOMENT_INF,
-                                       damping=0.0000001,
-                                       collision_type="player",
-                                       max_velocity=350)
+        self.player_sprite.center_x = self.position_x
+        self.player_sprite.center_y = self.position_y
+
+    def get_change_x(self,x):
+        return self.change_x
+
+    def set_change_x(self,x):
+        self.change_x = x
+
+    def get_change_y(self, y):
+        return self.change_y
+
+    def set_change_y(self, y):
+        self.change_y = y
 
     def get_hp(self):
         return self.hp
@@ -65,129 +67,10 @@ class Personaje(arcade.Sprite):
     def set_hp(self, new_hp):
         self.hp = new_hp
 
-    #def movement(self):
-
-    def draw_personaje(self):
+    def draw(self):
+        """ Draw everything """
         self.player_list.draw()
 
-class Protagonista(Personaje):
-
-    def __init__(self, sprite):
-        super().__init__(sprite)
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
-        self.physics_engine: Optional[PymunkPhysicsEngine] = None
-        self.player_list = None
-        self.player_sprite = arcade.Sprite(sprite, SPRITE_SCALING_PLAYER)
-        self.hp = None
-
-    def setup(self):
-        """ Set up everything """
-        # Create the sprite lists
-        self.player_list = arcade.SpriteList()
-        self.player_sprite.center_x = 250
-        self.player_sprite.center_y = 250
-        self.player_list.append(self.player_sprite)
-        self.physics_engine.add_sprite(self.player_sprite,
-                                       friction=0.6,
-                                       moment=PymunkPhysicsEngine.MOMENT_INF,
-                                       damping=0.0000001,
-                                       collision_type="player",
-                                       max_velocity=350)
-
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-
-        if key == arcade.key.W:
-            self.up_pressed = True
-        elif key == arcade.key.S:
-            self.down_pressed = True
-        elif key == arcade.key.A:
-            self.left_pressed = True
-        elif key == arcade.key.D:
-            self.right_pressed = True
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.W:
-            self.up_pressed = False
-        elif key == arcade.key.S:
-            self.down_pressed = False
-        elif key == arcade.key.A:
-            self.left_pressed = False
-        elif key == arcade.key.D:
-            self.right_pressed = False
-
-    def on_update(self, delta_time):
-      """ Movement """
-
-      # Calculate speed based on the keys pressed
-      self.player_sprite.change_x = 0
-      self.player_sprite.change_y = 0
-
-      if self.up_pressed and not self.down_pressed:
-          force = (0, PLAYER_MOVE_FORCE)
-          self.physics_engine.apply_force(self.player_sprite, force)
-      elif self.down_pressed and not self.up_pressed:
-          force = (0, -PLAYER_MOVE_FORCE)
-          self.physics_engine.apply_force(self.player_sprite, force)
-      if self.left_pressed and not self.right_pressed:
-          self.player_sprite.change_x = -MOVEMENT_SPEED
-          force = (-PLAYER_MOVE_FORCE, 0)
-          self.physics_engine.apply_force(self.player_sprite, force)
-      elif self.right_pressed and not self.left_pressed:
-          force = (PLAYER_MOVE_FORCE, 0)
-          self.physics_engine.apply_force(self.player_sprite, force)
-
-      # --- Move items in the physics engine
-      self.physics_engine.step()
-
-    def on_draw(self):
-       """ Draw everything """
-       arcade.start_render()
-       self.player_list.draw()
-
-   #def update_protagonista(self):
-   #    # Calculate speed based on the keys pressed
-   #    self.player_sprite.change_x = 0
-   #    self.player_sprite.change_y = 0
-
-   #    if self.up_pressed and not self.down_pressed:
-   #        force = (0, PLAYER_MOVE_FORCE)
-   #        self.physics_engine.apply_force(self.player_sprite, force)
-   #    elif self.down_pressed and not self.up_pressed:
-   #        force = (0, -PLAYER_MOVE_FORCE)
-   #        self.physics_engine.apply_force(self.player_sprite, force)
-   #    if self.left_pressed and not self.right_pressed:
-   #        self.player_sprite.change_x = -MOVEMENT_SPEED
-   #        force = (-PLAYER_MOVE_FORCE, 0)
-   #        self.physics_engine.apply_force(self.player_sprite, force)
-   #    elif self.right_pressed and not self.left_pressed:
-   #        force = (PLAYER_MOVE_FORCE, 0)
-   #        self.physics_engine.apply_force(self.player_sprite, force)
-
-   #    # --- Move items in the physics engine
-   #    #self.physics_engine.step()
-
-
-   #def draw_protagonista(self):
-   #    self.player_list.draw()
-
-# --- Pymunk Physics Engine Setup ---
-
-# The default damping for every object controls the percent of velocity
-# the object will keep each second. A value of 1.0 is no speed loss,
-# 0.9 is 10% per second, 0.1 is 90% per second.
-# For top-down games, this is basically the friction for moving objects.
-# For platformers with gravity, this should probably be set to 1.0.
-# Default value is 1.0 if not specified.
-# damping = 0.7
-
-# Set the gravity. (0, 0) is good for outer space and top-down.
-# gravity = (0, 0)
-
-# Create the physics engine
-# self.physics_engine = PymunkPhysicsEngine(damping=damping,gravity=gravity)
+    def update(self):
+        self.player_sprite.center_y = self.player_sprite.center_y + self.change_y
+        self.player_sprite.center_x = self.player_sprite.center_x + self.change_x
